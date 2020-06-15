6edda2eba3b959f5d1afca34455d266e78240279
nehagcryp <sneha.gurav@cryptextechnologies.com>
2020-06-15 10:32:22 +0530

    Added my project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+     if current_user.admin == true
+       admin_root_path
+  else
+     root_path(current_user)
+    end
+end
+
+
 end
